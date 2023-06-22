d6efab3eef38b96066b28a04c3fdf0d706578e77
MrWagyu <quincy.waugh@optusnet.com.au>
2023-06-22 20:53:26 +1000

    ^

--- src/main/java/net/mrwagyu/tutorialmod/item/ModItems.java
+++ src/main/java/net/mrwagyu/tutorialmod/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.mrwagyu.tutorialmod.item;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.mrwagyu.tutorialmod.TutorialMod;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static  final RegistryObject<Item> WAGYU = ITEMS.register("wagyu", () > new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
 }
